ec06b82b66f2b0d8eda3c38451d2446cb7818839
against-v <against.vegetables@gmail.com>
2021-01-05 23:46:23 +0300

    add server, add nodemon

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -6,6 +6,46 @@ const fs = require(`fs`).promises;
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
+const { HttpCode } = require(`../../constants`);
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
 
 module.exports = {
   name: `--server`,
