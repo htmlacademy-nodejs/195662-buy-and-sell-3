8ad9e1c5564986de03fdcd812db92dfd331c0036
against-v <against.vegetables@gmail.com>
2021-01-08 18:09:49 +0300

    add offers templates, fix main routes

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -6,6 +6,6 @@ const mainRouter = new Router();
 mainRouter.get(`/`, (req, res) => res.render(`main`));
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.render(`search`));
+mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
 
 module.exports = mainRouter;
