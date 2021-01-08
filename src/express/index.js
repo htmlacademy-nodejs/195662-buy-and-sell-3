97739ca2f4e3d4ee289a6eceeb8af460af4ecc45
against-v <against.vegetables@gmail.com>
2021-01-08 16:11:54 +0300

    add static

--- src/express/index.js
+++ src/express/index.js
@@ -4,12 +4,16 @@ const express = require(`express`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const offersRoutes = require(`./routes/offers-routes`);
+const path = require(`path`);
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.listen(DEFAULT_PORT);
