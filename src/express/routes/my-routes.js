67709c99119c86cf719591e68e05ff35c38f1f0a
against-v <against.vegetables@gmail.com>
2021-01-08 17:45:50 +0300

    fix my-routes.js

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -4,6 +4,6 @@ const {Router} = require(`express`);
 const myRouter = new Router();
 
 myRouter.get(`/`, (req, res) => res.render(`my-tickets`));
-myRouter.get(`/comments`, (req, res) => res.render(`comments--empty`));
+myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRouter;
