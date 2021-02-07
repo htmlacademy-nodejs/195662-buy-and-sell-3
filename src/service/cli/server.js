365227132989924d7e90f19b9a2fc7aefcaec25a
against-v <against.vegetables@gmail.com>
2021-02-07 13:48:00 +0300

    data-srvice

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,26 +2,13 @@
 
 const express = require(`express`);
 const chalk = require(`chalk`);
-const fs = require(`fs`).promises;
 
 const DEFAULT_PORT = 3000;
-const FILENAME = `mocks.json`;
 const {HttpCode} = require(`../../constants`);
 
 const app = express();
 
 app.use(express.json());
-
-app.get(`/offers`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-
-    res.json(mocks);
-  } catch (err) {
-    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
-  }
-});
 
 app.use((req, res) => res
   .status(HttpCode.NOT_FOUND)
