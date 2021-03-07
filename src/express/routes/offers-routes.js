110589b3ec4504f27dd97022e57e33bdb083733c
against-v <against.vegetables@gmail.com>
2021-03-07 23:16:58 +0300

    /offers/edit: прикрутил запрос на получение оффера и категорий, исправил шаблон

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -2,10 +2,19 @@
 
 const {Router} = require(`express`);
 const offersRouter = new Router();
+const api = require(`../api`).getAPI();
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  // Из-за бага в pug вместо offer в express части приложения будет использоваться suggestion
+  const [suggestion, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`offers/ticket-edit`, {suggestion, categories});
+});
 offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
 
 module.exports = offersRouter;
