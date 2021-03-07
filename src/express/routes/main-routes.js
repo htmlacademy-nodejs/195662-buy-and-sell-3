2b2688d617f57a451e5c3ee8c6fa08c1079edd7c
against-v <against.vegetables@gmail.com>
2021-03-07 23:42:19 +0300

    /search: прикрутил запрос, исправил шаблон

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -11,6 +11,14 @@ mainRouter.get(`/`, async (req, res) => {
 });
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
-mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
+mainRouter.get(`/search`, async (req, res) => {
+  try {
+    const {search} = req.query;
+    const results = await api.search(search);
+    res.render(`search-result`, {results});
+  } catch (error) {
+    res.render(`search-result`, {results: []});
+  }
+});
 
 module.exports = mainRouter;
