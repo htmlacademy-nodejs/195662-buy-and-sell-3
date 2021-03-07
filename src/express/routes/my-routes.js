3dfb36257eaa9ddffa6b1ed99b267be7f30d3db0
against-v <against.vegetables@gmail.com>
2021-03-07 22:18:48 +0300

    /my: прикрутил запрос на получение офферов, исправил шаблон

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,8 +2,13 @@
 
 const {Router} = require(`express`);
 const myRouter = new Router();
+const api = require(`../api`).getAPI();
 
-myRouter.get(`/`, (req, res) => res.render(`my-tickets`));
+myRouter.get(`/`, async (req, res) => {
+  // Из-за бага в pug вместо offer в express части приложения будет использоваться suggestion
+  const suggestions = await api.getOffers();
+  res.render(`my-tickets`, {suggestions});
+});
 myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRouter;
